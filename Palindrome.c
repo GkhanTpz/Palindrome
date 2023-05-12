1cec987a1ef1fe4afda6cec64f3282a4e07d4c69
Gökhan TOPUZ <gokhan061@msn.com>
2023-05-12 19:31:34 +0300

    Create Palindrome.c

---
+++ Palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_SIZE 50
+
+// A sentence or word checked by enum must only be Palindrome or Non-Palindrome 
+typedef enum 
+{
+    PALINDROME,
+    NON_PALINDROME
+} CharControl;
+
+int main()
+{
+    char mySentence[MAX_SIZE];
+    char *ch1, *ch2;
+
+    printf("Please enter a sentence: ");
+    fgets(mySentence, MAX_SIZE, stdin);
+
+    // Remove the newline character from 'mySentence'
+    mySentence[strcspn(mySentence, "\n")] = '\0';
+
+    // 'ch2' holds the last character of 'mySentence'
+    ch2 = mySentence + strlen(mySentence) - 1;
+
+    CharControl Status;
+    for (ch1 = mySentence; ch1 < ch2; ch1++, ch2--)
+    {
+        if (*ch1 != *ch2)
+        {
+            Status = NON_PALINDROME;
+            break;
+        }
+        else
+        {
+            Status = PALINDROME;
+        }
+    }
+
+    Status == PALINDROME
+    ? printf("The sentence '%s' is a palindrome.\n", mySentence)
+    : printf("The sentence '%s' is not a palindrome.\n", mySentence);
+
+    return 0;
+}
